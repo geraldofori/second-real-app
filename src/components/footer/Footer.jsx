b686ed509b85848f6fd9060b7e1c0202aa947db6
geraldofori <oforige@gmail.com>
2024-04-09 15:42:35 +0000

    updates to the footer and navbars

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,12 @@
-
+import styles from "./footer.module.css"
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>Geraldev</div>
+      <div className={styles.text}>
+        Gerald creative thoughts agency .All rights reserved
+      </div>
+    </div>
   );
 }
 
