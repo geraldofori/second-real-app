c3c8b893dd784a06498d0f77cbf231a544b66b00
geraldofori <oforige@gmail.com>
2024-04-10 12:17:22 +0000

    implementation of the contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,26 @@
+import styles from "./contact.module.css"
+import Image from "next/image"
+
 const Contact = () => {
     return (
-      <div>Contact page</div>
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" alt="Contact image" fill className={styles.img}/>
+        </div>
+        <div className={styles.formContainer}>
+          <form action="" className={styles.form}>
+            <input type="text" placeholder="Name and Surname" />
+            <input type="text" placeholder="Email address" />
+            <input type="text" placeholder="Phone #(optional)" />
+            <textarea name="" id="" cols="30" rows="10" placeholder="Message">
+
+            </textarea>
+            <button>Send</button>
+
+          </form>
+        </div>
+        
+      </div>
     );
   }
   
