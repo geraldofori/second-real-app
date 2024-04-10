e0b466affbd091e6f6ec73bbb6394b3e413d5b53
geraldofori <oforige@gmail.com>
2024-04-10 12:16:38 +0000

    implementation of the postcard component

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -7,7 +7,7 @@ const PostCard = () => {
     <div className={styles.container}>
         <div className={styles.top}>
             <div className={styles.imgContainer}>
-                <Image src=""/>
+                <Image src="https://images.pexels.com/photos/20074173/pexels-photo-20074173/free-photo-of-a-group-of-envelopes-with-the-word-love-on-them.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className={styles.imgContainer} fill/>
             </div>
             <span className={styles.date}>01.01.2024</span>
         </div>
